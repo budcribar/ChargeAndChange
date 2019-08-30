d731ad592a208a8eac2765e9faecbe1e86b0ac40
Bud Cribar <budcribar@msn.com>
2019-08-30 12:03:44 -0600

    menu work

---
+++ CCWebSite/ClientApp/src/app/mission/mission.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-component',
+  templateUrl: './mission.component.html'
+})
+export class MissionComponent {
+  
+}
