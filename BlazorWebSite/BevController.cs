95bb6105eac8c1dbcad35a481cdda758ec7f2d61
Bud Cribar <budcribar@msn.com>
2020-07-26 14:45:45 -0600

    add fetch data

---
+++ BlazorWebSite/BevController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCWebSite.Controllers
+{
+    public class ChartData
+    {
+        public string Name { get; set; }
+        public double? Y { get; set; }
+    }
+
+    
+    public class BEVController 
+    {
+        private readonly IDocumentDBRepository<EVSpecs> respository;
+        public BEVController(IDocumentDBRepository<EVSpecs> Respository)
+        {
+            this.respository = Respository;
+        }
+
+        //[HttpPost]
+        //[ActionName("Edit")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> EditAsync([Bind("Id,Name,Description,Completed")] EVSpecs item)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        await respository.UpdateItemAsync(item.Id, item);
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(item);
+        //}
+
+       
+        public async void Delete(string id)
+        {
+            await respository.DeleteItemAsync(id);
+        }
+
+        
+        public async void Patch(string id,EVSpecs bev)
+        {
+            if (bev == null) return;
+            bev.Id = id;
+            await respository.UpdateItemAsync(id, bev);
+        }
+
+        
+        public async void Post(EVSpecs bev)
+        {
+            if (bev == null) return;
+            bev.Id = Guid.NewGuid().ToString();
+            await respository.CreateItemAsync(bev);
+        }
+
+        private double? GetValue(EVSpecs evspec, string spec)
+        {
+            var o =  typeof(EVSpecs).GetProperty(spec).GetValue(evspec);
+            if (o == null) return null;
+
+            return ((IConvertible)o).ToDouble(null);
+        }
+
+        
+        public IEnumerable<ChartData> Spec(string spec, Boolean availableOnly)
+        {
+            return respository.GetItemsAsync(x => true).Result.Where(y => !availableOnly || y.Available).Select(x => new ChartData { Name = x.Manufacturer + ' ' + x.Model, Y = GetValue(x, spec) }).Where(x => x.Y != null).OrderBy(x => x.Y);
+        }
+
+       
+        public Task<IEnumerable<EVSpecs>> EVSpecs()
+        {
+            return respository.GetItemsAsync(x => true);
+        }
+
+        
+    }
+}
