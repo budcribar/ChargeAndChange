db8672732cd8678866efad06df157286cef87389
Bud Cribar <budcribar@msn.com>
2020-12-06 07:57:28 -0700

    cleanup

--- CCWebSite/ClientApp/src/app/mission/mission.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mission-component',
-  templateUrl: './mission.component.html'
-})
-export class MissionComponent {
-  
-}
